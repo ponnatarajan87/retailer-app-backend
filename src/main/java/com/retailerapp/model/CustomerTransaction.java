aa4cfb3475f0a3fb816eff636ccb5d94ca19f65e
Natraj <ponnatarajan87@gmail.com>
2022-08-29 16:52:21 +0530

    backup

--- src/main/java/com/retailerapp/model/CustomerTransaction.java
+++ src/main/java/com/retailerapp/model/CustomerTransaction.java
@@ -2,9 +2,11 @@ package com.retailerapp.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CustomerTransaction {
     private String customerName;
     private String transactionDate;
